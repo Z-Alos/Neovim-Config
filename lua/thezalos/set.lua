ceb87af964c2cf50085533f7e37add24dba1db52
zalos <satya123m4@gmail.com>
2025-08-17 09:48:49 +0530

    fix: no autocomment next line

--- lua/thezalos/set.lua
+++ lua/thezalos/set.lua
@@ -32,5 +32,10 @@ vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
 
-
-
+-- Disable auto comment continuation globally
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt.formatoptions:remove { "c", "r", "o" }
+    end,
+})
